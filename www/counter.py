aeddf00bcd221451342a4a05d8e770f144c0b619
Roman Komkov <r.komkov@gmail.com>
2016-08-25 17:22:03 +0200

    Let app get REDIS IP from env

--- www/counter.py
+++ www/counter.py
@@ -6,10 +6,9 @@ import redis
 import socket
 
 
-
 app = Flask(__name__)
 hostname = socket.gethostname()
-redis = redis.Redis("redis")
+redis = redis.Redis(os.getenv("REDIS_HOST", "redis"))
 
 
 if "DEBUG" in os.environ:
@@ -19,19 +18,19 @@ if "DEBUG" in os.environ:
 @app.errorhandler(500)
 def error(e):
     return render_template('error.html',
-        hostname=hostname, error=e), 500
+                           hostname=hostname, error=e), 500
 
 
 @app.route("/")
 def index():
     redis.zincrby("counters", hostname)
     counters = redis.zrevrange("counters", 0, -1, withscores=True)
-    counters = [ (s.decode(), int(i)) for (s,i) in counters ]
+    counters = [(s.decode(), int(i)) for (s, i) in counters]
     thiscount = int(redis.zscore("counters", hostname))
-    totalcount = sum(i for (s,i) in counters)
-    return render_template( "index.html",
-        hostname=hostname, counters=counters,
-        thiscount=thiscount, totalcount=totalcount)
+    totalcount = sum(i for (s, i) in counters)
+    return render_template("index.html",
+                           hostname=hostname, counters=counters,
+                           thiscount=thiscount, totalcount=totalcount)
 
 
 @app.route("/assets/<path:path>")
@@ -42,3 +41,4 @@ def assets(path):
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 
+#  vim: set et fenc=utf-8 ft=python sts=4 sw=4 ts=4 tw=79 :
